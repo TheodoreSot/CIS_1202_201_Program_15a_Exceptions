08b737ed4dfb2eabe33f110b88d71df05fa82893
Theodore Sotirelis <tg.sotirelis@gmail.com>
2023-12-01 22:07:07 -0500

    Added function and driver for function. Worked correctly in Xcode.

--- CIS_1202_201_Program_15a_Exceptions/main.cpp
+++ CIS_1202_201_Program_15a_Exceptions/main.cpp
@@ -1,8 +1,67 @@
+#include <string>
 #include <iostream>
 using namespace std;
 
+char character(char start, int offset);
+
 int main()
 {
+    try
+    {
+        cout << character('a',1) << endl;
+    }
+    catch(string exceptionString)
+    {
+        cout << exceptionString << endl;
+    }
+    
+    try
+    {
+        cout << character('a',-1) << endl;
+    }
+    catch(string exceptionString)
+    {
+        cout << exceptionString << endl;
+    }
+    
+    try
+    {
+        cout << character('Z',-1) << endl;
+    }
+    catch(string exceptionString)
+    {
+        cout << exceptionString << endl;
+    }
+    
+    try
+    {
+        cout << character('?',5) << endl;
+    }
+    catch(string exceptionString)
+    {
+        cout << exceptionString << endl;
+    }
     
     return 0;
+}
+
+char character(char start, int offset)
+{
+    char letterWanted;
+    string invalidRangeExpection = "Character wanted is not a letter";
+    string invalidCharacterExcpeption= "Character entered was not a letter.";
+    
+    if(!isalpha(start))
+    {
+        throw invalidCharacterExcpeption;
+    }
+    
+    if(!isalpha(start+offset))
+    {
+        throw invalidRangeExpection;
+    }
+    
+    letterWanted = start+offset;
+
+    return letterWanted;
 }
